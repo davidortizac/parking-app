99a7db70427aa9b6b111c724c01ff21447afe60e
davidortizac <david.ortizac@hotmail.com>
2025-08-14 19:02:17 -0500

    Refactor Docker Compose configuration and update frontend package settings

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // redirige llamadas a /api al backend FastAPI
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
